3a377206ce5520199950028ece69c9f943555bde
Damen Turnbull <info@fando.com.au>
2015-04-14 22:47:23 +1000

    Dynamic tax rule repetition.

--- app.js
+++ app.js
@@ -3,20 +3,28 @@
 
   app.controller('TaxController', function(){
     this.rules = rules;
+
+    this.getBaseValue = function(value) {
+      return value;
+    };
+
   });
 
   var rules = [{
-    ruleId:   1,
-    minValue: 0,
-    maxValue: 18200
+    ruleId:             1,
+    minValue:           0,
+    maxValue:           18200,
+    valuePerDollarOver: 0.19
   }, {
-    ruleId:   2,
-    minValue: 18201,
-    maxValue: 37000
+    ruleId:             2,
+    minValue:           18201,
+    maxValue:           37000,
+    valuePerDollarOver: 0.325
   }, {
-    ruleId:   3,
-    minValue: 37001,
-    maxValue: 80000
+    ruleId:             3,
+    minValue:           37001,
+    maxValue:           80000,
+    valuePerDollarOver: 0.37
   }];
 
 })();
