72cfd658ff7e7a209b0f025c9ca6bd4b0581db0a
Damen Turnbull <info@fando.com.au>
2015-04-17 10:57:32 +1000

    Minor updates.

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 (function() {
   var app = angular.module('taxRules', []);
 
-  app.controller('TaxController', function(){
+  app.controller('TaxController', function($filter){
     this.rules = rules;
 
     this.getBaseValue = function(value) {
       return value;
     };
 
+    this.addRule = function() {
+      // console.log(this.rules);
+      var rule = {
+        ruleId:             this.ruleId_next, 
+        minValue:           this.minValue, 
+        maxValue:           this.maxValue, 
+        valuePerDollarOver: this.valuePerDollarOver};
+
+      this.rules.push(rule);
+      // Reset next id
+      this.ruleId_next = this.minValue = this.maxValue = this.valuePerDollarOver = null;
+      this.getNextMinValue();
+      this.getNextId();
+    };
+
+    this.getNextId = function() {
+      this.ruleId_next = this.rules.length + 1;
+    };
+
+    this.getNextMinValue = function() {
+      var last = this.rules[this.rules.length - 1];
+      this.minValue = last.maxValue + 1;
+    }
+
+    this.getNextMinValue = function() {
+      var last = this.rules[this.rules.length - 1];
+      this.minValue = last.maxValue + 1;
+      // this.minValue = $filter('currency')(last.maxValue + 1, '$', 0);
+    }
+
+    this.getNextMaxValue = function() {
+      // this.maxValue = $filter('currency')(0, '$', 0);
+    }
+
+    // Initiate
+    this.getNextId();
+    this.getNextMinValue();
+    this.getNextMaxValue();
   });
 
   var rules = [{
